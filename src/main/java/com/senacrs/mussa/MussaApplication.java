535383fe43dfa1ce1e588ff90a650b2ec7601a42
Angelo Luz <angelogl@gmail.com>
2018-01-20 00:39:54 -0200

    First commit

---
+++ src/main/java/com/senacrs/mussa/MussaApplication.java
@@ -0,0 +1,12 @@
+package com.senacrs.mussa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MussaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MussaApplication.class, args);
+	}
+}
